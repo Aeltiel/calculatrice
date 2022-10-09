0cb6b1e92d2988014df0f393c2d751e7f53bde40
Ingrid Stemer <ingrid.stemer@live.fr>
2022-10-09 15:38:47 +0200

    claculatrice finish

--- calculatrice.js
+++ calculatrice.js
@@ -1,41 +1,17 @@
-let total = 0;
+const buttons = document.querySelectorAll(".btn");
+const reSet = document.querySelector(".reset");
+const result = document.getElementById("result");
 
-function addition (x){
-    total += x;
-    return total;
-}
-
-function soustraction (x){
-    total -= x;
-    return total;
-}
-
-function multiplication (x){
-    if(total===0){
-        return (total=x);
-    }else{
-        total *= x;
-        return total;
-    }
-}
-
-function division (x){
-    if(total===0){
-        return (total=x);
-    }else{
-        total /= x;
-        return total;
-    }
-}
-
-function reset(){
-    total =0;
-}
-
-// Ajout des evenements on click
-const boutton = document.querySelectorAll("button");
-const result = document.querySelector(".calc__reset");
-
-boutton.addEventListener("click", ()=>{
-    result.textContent = "test";
+buttons.forEach(button => {
+    button.addEventListener('click',(action)=>{
+        result.textContent += action.target.id;
+    })
 });
+
+egal.addEventListener('click', ()=>{
+    result.textContent = eval(result.textContent);
+})
+
+reSet.addEventListener('click', ()=>{
+    result.textContent = "";
+})
